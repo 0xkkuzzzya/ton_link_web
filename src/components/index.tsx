adb7c8d1b422efaae661dcb9cdef32e2e218ce1d
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-04-11 21:28:22 +0500

    feat: init

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 1000px;
+    margin: 0 auto;
+`
+
+
+export const MainIndex = () => {
+    return(
+        <Container>
+
+        </Container>
+    )
+}
